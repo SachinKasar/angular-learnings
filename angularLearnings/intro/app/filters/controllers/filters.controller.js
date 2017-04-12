8f97de7dca917881c21e75637b7d3a5c6a227cf5
SachinKasar <kasarsac@WL308554.ap.tieto.com>
2017-04-12 16:55:11 +0530

    Adding Filters and Bootstrap changes

---
+++ angularLearnings/intro/app/filters/controllers/filters.controller.js
@@ -0,0 +1,60 @@
+filtersModule.controller("FiltersController", FiltersController);
+
+
+function FiltersController(filtersService) {
+    // this = $scope;  // Angular do this by default for Controller As syntax 
+    var self = this
+    self.updateFlag = false;
+    self.addFlag = false;
+    self.employees = {};
+    self.today = new Date();
+
+
+    filtersService.getEmployees().then(function (data) { // READ
+            self.employees = data;
+            self.successFetchNotificationMessage = "Fetched Employee Details Successfully !!!"
+        },
+        function () {
+           self.errorFetchNotificationMessage = "Unable to fetch eployees details. Please verify json-server !!!"
+        }
+    );
+
+
+    this.showSelectedEmployee = function (index) {
+        this.successFetchNotificationMessage = undefined;
+        this.errorFetchNotificationMessage = undefined;
+        this.selectedEmployee = self.employees[index];
+
+    }
+
+    this.toggleUpdateFlag = function () {
+        this.updateFlag = !this.updateFlag;
+    }
+
+    this.updateEmployee = function () {
+        this.toggleUpdateFlag();
+        if(self.addFlag) { // CREATE
+            filtersService.addEmployee(this.selectedEmployee);
+            self.addFlag = false;
+        } else { // UPDATE
+            filtersService.updateEmployee(this.selectedEmployee);
+        }
+          
+    }
+    
+    
+    this.addEmployee = function () {
+        this.updateFlag = true;
+        this.addFlag = true;
+        this.selectedEmployee = {
+            "id": new Date().toTimeString()
+        };
+   }
+    
+   this.deleteEmployee = function () { // DELETE
+       filtersService.deleteEmployee(this.selectedEmployee);
+   }
+    
+
+
+}
---
+++ angularLearnings/intro/app/filters/services/filters.service.js
@@ -0,0 +1,37 @@
+filtersModule.service('filtersService', function($http) {  // dependency for $http service
+    
+    
+    this.getEmployees = function() {
+        var promise1 = $http.get("http://localhost:3000/employees");
+        var promise2 = promise1.then(function(response) {
+             return  response.data;
+        });
+        
+        return promise2;
+    }
+    
+    this.updateEmployee = function(employeeDetails) {
+         return $http.put("http://localhost:3000/employees/"+employeeDetails.id,employeeDetails)
+                .then(function(response) {
+                    // console.log("PUT Done --- " + response);
+                });
+    }
+    
+    this.addEmployee = function(employeeDetails) {
+        return $http.post("http://localhost:3000/employees/",employeeDetails)
+                .then(function(response) {
+                    // console.log("Add Done --- " + response);
+                });
+    }
+    
+    this.deleteEmployee = function(employeeDetails) {
+        return $http.delete("http://localhost:3000/employees/"+employeeDetails.id,employeeDetails)
+                .then(function(response) {
+                    // console.log("deleteEmployee Done --- " + response);
+                });
+    }
+    
+    
+    
+    
+});
