b90aca774d7944d85384fe98911cb257a3ab4065
SachinKasar <kasarsac@WL308554.ap.tieto.com>
2017-04-14 12:13:10 +0530

    Examples updated

---
+++ angularLearnings/intro/app/common/controller/digest.controller.js
@@ -0,0 +1,44 @@
+
+var digestModule = angular.module("digestModule", []);
+digestModule.controller("DigestController", function ($scope) {
+    $scope.name = 'First Controller';
+    $scope.topic = 'Angular';
+    $scope.data = { time: new Date() };
+    $scope.random = Math.random;
+
+    $scope.getName = function () {
+        return $scope.name;
+    };
+
+    $scope.reverseName = function () {
+        $scope.topic = $scope.topic.split("").reverse().join("");
+    };
+
+    $scope.getTopic = function () {
+        console.log('dirty-checking');
+        return $scope.topic;
+    };
+    
+    $scope.updateTime = function () {
+        $scope.data.time = new Date();
+    }
+
+    $scope.$watch(function (scope) {
+            return scope.data.time
+        },
+        function (newValue, oldValue) {
+            console.log("newValue -- " + newValue +" oldValue--- " + oldValue );
+        }
+    );
+
+     
+        
+    document.getElementById("updateTimeButton").addEventListener('click', function () {
+            $scope.$apply(function () {
+                console.log("Digest Cycle Started");
+                $scope.data.time = new Date();
+                //$scope.$digest();
+            })
+        });
+    
+});
